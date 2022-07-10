19598e0b79070a4c70816b4ec629885caee345b0
JuneBridge <44526783+JuneBridge03@users.noreply.github.com>
2022-07-10 15:33:57 +0900

    블루투스 통신 위치 반영 코드 추가

--- app/src/main/java/pikachu/blackbox/DangerCheckingService.java
+++ app/src/main/java/pikachu/blackbox/DangerCheckingService.java
@@ -3,11 +3,15 @@ package pikachu.blackbox;
 import static pikachu.blackbox.ExitActivity.GPSsRef;
 import static pikachu.blackbox.ExitActivity.locationManager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -17,7 +21,11 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+
+import java.util.Set;
 import java.util.function.Function;
 
 public class DangerCheckingService extends Service {
@@ -27,12 +35,11 @@ public class DangerCheckingService extends Service {
     boolean isRun = false;
 
     SensorManager SensorManager;
-
     Sensor GgyroSensor = null;
-
     SensorEventListener SListen;
-
     float x, y, z;
+
+    BluetoothAdapter mBluetoothAdapter;
 
     Thread thread = new Thread(() -> {
 
@@ -48,10 +55,6 @@ public class DangerCheckingService extends Service {
 
                 @SuppressLint("MissingPermission") Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                loc.getLatitude();
-
-                loc.getLongitude();
-
                 Function<Location, Double> distance_f = (location) -> {
 
                     Double x = location.getLatitude();
@@ -63,28 +66,28 @@ public class DangerCheckingService extends Service {
                     Double x_2 = ExitActivity.final_lat.latitude;
                     Double y_2 = ExitActivity.final_lat.longitude;
 
-                    return Math.abs((x_2 - x_1) * y - (y_2 - y_1) * (x - x_1) - y_1 * (x_2 - x_1)) / Math.pow(Math.pow(x_2 - x_1, 2) + Math.pow(y_2 - y_1,2), 1/2);
+                    return Math.abs((x_2 - x_1) * y - (y_2 - y_1) * (x - x_1) - y_1 * (x_2 - x_1)) / Math.pow(Math.pow(x_2 - x_1, 2) + Math.pow(y_2 - y_1, 2), 1 / 2);
                 };
 
                 String reason = "";
 
                 //경로 이탈 및 가만히 있는 가를 확인
 
-                if(distance_f.apply(loc) >= 0.01){ // 경로 이탈 확인
+                if (distance_f.apply(loc) >= 0.01) { // 경로 이탈 확인
                     reason = "지정된 경로와 너무 멉니다.";
-                }else{ // 움직임 미감지 확인
-                    if(last_loc != null){
-                        Double distance = Math.pow(Math.pow(loc.getLatitude() - last_loc.getLatitude(), 2) + Math.pow(loc.getLongitude() - last_loc.getLongitude(),2), 1/2);
+                } else { // 움직임 미감지 확인
+                    if (last_loc != null) {
+                        Double distance = Math.pow(Math.pow(loc.getLatitude() - last_loc.getLatitude(), 2) + Math.pow(loc.getLongitude() - last_loc.getLongitude(), 2), 1 / 2);
 
                         last_loc = loc;
 
-                        if(System.currentTimeMillis() - time_location >= 60 * 1000){
-                            if(distance >= 0.001){
+                        if (System.currentTimeMillis() - time_location >= 60 * 1000) {
+                            if (distance >= 0.001) {
                                 reason = "동작 감지 불가";
                             }
                             time_location = System.currentTimeMillis();
                         }
-                    }else{
+                    } else {
                         last_loc = loc;
                     }
                 }
@@ -92,6 +95,19 @@ public class DangerCheckingService extends Service {
 
                 //TODO 자이로센서 확인
 
+                //블루투스 설정
+                @SuppressLint("MissingPermission") Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+
+                if(devices.size() > 0){
+                    for(BluetoothDevice device : devices){
+                        @SuppressLint("MissingPermission") String[] a = device.getName().split("-");
+                        if(a[0].equals("Pikachu")){
+                            loc.setLatitude(Double.parseDouble(a[1]));
+                            loc.setLatitude(Double.parseDouble(a[2]));
+                            break;
+                        }
+                    }
+                }
 
                 if (!reason.equals("")){ // 위험 확인 시 팝업 띄우기
                     Bundle bun = new Bundle();
@@ -135,6 +151,22 @@ public class DangerCheckingService extends Service {
         SListen = new GyroscopeListener();
 
         SensorManager.registerListener(SListen, GgyroSensor, android.hardware.SensorManager.SENSOR_DELAY_UI);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void bluetoothOn() {
+        if(mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), "블루투스가 이미 활성화 되어 있습니다.", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "블루투스가 활성화 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
